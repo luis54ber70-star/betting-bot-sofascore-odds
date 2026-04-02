605cc9b4215b597673baf274919b4443379741ba
luis54ber70-star <luis54ber.70@gmail.com>
2026-04-01 22:31:26 -0600

    Delete run_local.py

--- run_local.py
+++
@@ -1,7 +0,0 @@
-from src.live_scanner import generate_picks  # o el que quieras
-from src.notifier import send_telegram
-
-picks = generate_picks()
-print(picks)
-if picks:
-    send_telegram("\n".join(picks))
